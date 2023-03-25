0de7429a61c867cb062324926e9c6b3f1ead00d0
D3vlin <alexisdq16@gmail.com>
2023-03-25 11:25:41 -0500

    added boards component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
       colors: {
         success: colors.green,
         primary: colors.blue,
-      }
+        danger: colors.red,
+      },
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2x1': '1536px',
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
