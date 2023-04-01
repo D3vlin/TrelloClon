59c26fa9702210b84cce46101ecddc877222b99c
D3vlin <alexisdq16@gmail.com>
2023-04-01 10:10:21 -0500

    added columns

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
 
-import { ToDo } from "../../models/todo.model";
+import { ToDo, Column } from "../../models/todo.model";
 
 @Component({
   selector: 'app-board',
@@ -19,40 +19,61 @@ import { ToDo } from "../../models/todo.model";
   ]
 })
 export class BoardComponent {
-  todos: ToDo[] = [
+  columns: Column[] = [
     {
-      id: '1',
-      title: 'Task 1'
+      title: 'ToDo',
+      todos: [
+        {
+          id: '1',
+          title: 'Task 1'
+        },
+        {
+          id: '2',
+          title: 'Task 2'
+        },
+        {
+          id: '3',
+          title: 'Task 3'
+        },
+      ]
     },
     {
-      id: '2',
-      title: 'Task 2'
+      title: 'Doing',
+      todos: [
+        {
+          id: '4',
+          title: 'Task 4'
+        },
+      ]
     },
     {
-      id: '3',
-      title: 'Task 3'
-    },
-  ];
-
-  doing: ToDo[] = [
-    {
-      id: '4',
-      title: 'Task 4'
-    },
-  ];
-  done: ToDo[] = [
-    {
-      id: '5',
-      title: 'Task 5'
+      title: 'Done',
+      todos: [
+        {
+          id: '5',
+          title: 'Task 5'
+        },
+      ]
     },
   ];
 
   drop(event: CdkDragDrop<ToDo[]>) {
     if (event.previousContainer == event.container) {
-      moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     }
     else{
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
+  }
+
+  dropHorizontal (event: CdkDragDrop<Column[]>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
+
+  addColumn() {
+    this.columns.push({
+      title: 'New Column',
+      todos: [],
+    })
   }
 }
